27f7f8258db922bbe6a033966407aaa8218d56ec
don4ara <don4araa@gmail.com>
2026-01-31 18:55:37 +0500

    feat: добавлена система автообновления приложения

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="vite/client" />
 
 declare const __APP_VERSION__: string;
+
+interface Window {
+    ipcRenderer: {
+        send(channel: string, ...args: any[]): void;
+        on(channel: string, listener: (event: any, ...args: any[]) => void): void;
+        off(channel: string, ...args: any[]): void;
+        invoke(channel: string, ...args: any[]): Promise<any>;
+    };
+}
